3e06168cd13ddb0cd7a312407248afeb36cafd46
mirachin8 <chinm@bu.edu>
2025-05-04 18:07:22 +0000

    added training crates

--- src/trainer.rs
+++
@@ -1,6 +0,0 @@
-// trains the decision tree model
-use linfa::prelude::*;
-use linfa::Dataset;
-use linfa_trees::DecisionTree;
-use linfa::metrics::ToConfusionMatrix;
-use ndarray::{Array1, Array2};
